01bd8d6df8b75ee0a0681b50061de4df39eedbaf
Alexander <nikal@kth.se>
2015-12-20 12:54:11 +0100

    Small last changes

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import net.ucanaccess.jdbc.UcanaccessSQLException;
 
 import java.sql.SQLException;
 
